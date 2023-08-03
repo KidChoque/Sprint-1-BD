0afb3c818b2cc48bbff435c66802e28e357f48b6
KidChoque <lucassmachado1998@gmail.com>
2023-08-03 10:49:08 -0300

    Trabalhando no Ex_1_2 SQL Server

---
+++ SQLQuery1 - Copia.sql
@@ -0,0 +1,88 @@
+	USE Ex_1_2; 
+
+  
+
+CREATE TABLE Empresa 
+
+( 
+
+IdEmpresa INT PRIMARY KEY IDENTITY, 
+
+NomeEmpresa VARCHAR(100) NOT NULL, 
+
+) 
+
+  
+
+CREATE TABLE Cliente 
+
+( 
+
+IdCliente INT PRIMARY KEY IDENTITY, 
+
+Nome VARCHAR(100) NOT NULL, 
+
+CPF VARCHAR(11) NOT NULL, 
+
+) 
+
+  
+
+CREATE TABLE Modelos  
+
+( 
+
+IdModelos INT PRIMARY KEY IDENTITY, 
+
+NomeModelo VARCHAR(30) NOT NULL, 
+
+) 
+
+  
+
+CREATE TABLE Marcas 
+
+( 
+
+IdMarcas INT PRIMARY KEY IDENTITY, 
+
+NomeMarca VARCHAR(30) NOT NULL, 
+
+) 
+
+  
+
+CREATE TABLE Veiculos 
+
+( 
+
+IdVeiculos INT PRIMARY KEY IDENTITY, 
+
+IdEmpresa INT FOREIGN KEY REFERENCES Empresa(IdEmpresa), 
+
+IdModelos INT FOREIGN KEY REFERENCES Modelos(IdModelos), 
+
+IdMarcas INT FOREIGN KEY REFERENCES Marcas(IdMarcas), 
+
+Placa VARCHAR(7) NOT NULL, 
+
+) 
+
+  
+
+CREATE TABLE Aluguel 
+( 
+
+IdAluguel INT PRIMARY KEY IDENTITY, 
+
+IdVeiculos INT FOREIGN KEY REFERENCES Veiculos(IdVeiculos), 
+
+IdCliente INT FOREIGN KEY REFERENCES Cliente(IdCliente), 
+
+DataAluguel DATE,
+
+DataDevolucao DATE
+
+) 
+
+DROP TABLE Aluguel
